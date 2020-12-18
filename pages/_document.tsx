1694c822457898e078f0f1a06fbac0c7649962e6
Michael Albertz <m.albertz@sb-konzept.de>
2020-12-18 21:25:40 +0100

    chore: update dependencies
    next-pwa now works as expected with webpack 5

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -29,11 +29,6 @@ class MyDocument extends Document {
           <meta name="format-detection" content="telephone=no" />
           <meta name="mobile-web-app-capable" content="yes" />
           <meta name="theme-color" content="#FFFFFF" />
-          <meta
-            name="viewport"
-            content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover"
-          />
-
           <link
             rel="apple-touch-icon"
             sizes="180x180"
